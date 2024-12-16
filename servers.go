9ccb6da6e46eaaf9163071744e86e0f21cbb9d60
Sanchit Rk <sanchitrrk@gmail.com>
2024-12-16 16:02:53 +0530

    fix: linting and added json tagging for each fields, so anonymous struct inherits in custom MarshalJSON implementation.

--- servers.go
+++ servers.go
@@ -10,23 +10,23 @@ import (
 // Server represents a server registered in your Postmark account
 type Server struct {
 	// ID of server
-	ID int64
+	ID int64 `json:"ID"`
 	// Name of server
-	Name string
+	Name string `json:"Name"`
 	// APITokens associated with server.
 	APITokens []string `json:"ApiTokens"`
 	// ServerLink to your server overview page in Postmark.
-	ServerLink string
+	ServerLink string `json:"ServerLink"`
 	// Color of the server in the rack screen. Purple Blue Turquoise Green Red Yellow Grey
-	Color string
+	Color string `json:"Color"`
 	// SMTPAPIActivated specifies whether SMTP is enabled on this server.
 	SMTPAPIActivated bool `json:"SmtpApiActivated"`
 	// RawEmailEnabled allows raw email to be sent with inbound.
-	RawEmailEnabled bool
+	RawEmailEnabled bool `json:"RawEmailEnabled"`
 	// DeliveryType specifies the type of environment for your server: Live or Sandbox, defaults to Live
 	DeliveryType string `json:"DeliveryType"`
 	// InboundAddress is the inbound email address
-	InboundAddress string
+	InboundAddress string `json:"InboundAddress"`
 	// InboundHookURL to POST to every time an inbound event occurs.
 	InboundHookURL string `json:"InboundHookUrl"`
 	// BounceHookURL to POST to every time a bounce event occurs.
@@ -35,23 +35,24 @@ type Server struct {
 	OpenHookURL string `json:"OpenHookUrl"`
 	// PostFirstOpenOnly - If set to true, only the first open by a particular recipient will initiate the open webhook. Any
 	// subsequent opens of the same email by the same recipient will not initiate the webhook.
-	PostFirstOpenOnly bool
+	PostFirstOpenOnly bool `json:"PostFirstOpenOnly"`
 	// TrackOpens indicates if all emails being sent through this server have open tracking enabled.
-	TrackOpens bool
+	TrackOpens bool `json:"TrackOpens"`
 	// TrackLinks specifies link tracking in emails: None, HtmlAndText, HtmlOnly, TextOnly, defaults to "None"
 	TrackLinks string `json:"TrackLinks"`
 	// IncludeBounceContentInHook determines if bounce content is included in webhook.
 	IncludeBounceContentInHook bool `json:"IncludeBounceContentInHook"`
 	// InboundDomain is the inbound domain for MX setup
-	InboundDomain string
+	InboundDomain string `json:"InboundDomain"`
 	// InboundHash is the inbound hash of your inbound email address.
-	InboundHash string
+	InboundHash string `json:"InboundHash"`
 	// InboundSpamThreshold is the maximum spam score for an inbound message before it's blocked.
-	InboundSpamThreshold int64
-	// EnableSmtpApiErrorHooks specifies whether SMTP API Errors will be included with bounce webhooks.
-	EnableSmtpApiErrorHooks bool `json:"EnableSmtpApiErrorHooks"`
+	InboundSpamThreshold int64 `json:"InboundSpamThreshold"`
+	// EnableSMTPAPIErrorHooks specifies whether SMTP API Errors will be included with bounce webhooks.
+	EnableSMTPAPIErrorHooks bool `json:"EnableSmtpApiErrorHooks"`
 }
 
+// MarshalJSON customizes the JSON representation of the Server struct by setting default values for specific fields.
 func (s Server) MarshalJSON() ([]byte, error) {
 	type Aux Server
 
