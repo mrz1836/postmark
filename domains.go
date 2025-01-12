87a0ce382378be135bf0d7a416822885a10fe08d
Marcus Carey <mcarey@firebend.com>
2025-01-12 13:32:32 -0600

    chore: fix casing and add json tags

--- domains.go
+++ domains.go
@@ -10,47 +10,50 @@ import (
 // Domain contains the details of a domain in Postmark. https://postmarkapp.com/developer/api/domains-api
 type Domain struct {
 	// Name of the domain.
-	Name string `binding:"required"`
+	Name string `json:"Name" binding:"required"`
+	// Deprecated: See our [blog post](https://postmarkapp.com/blog/why-we-no-longer-ask-for-spf-records) to learn
+	// why this field was deprecated.
+	SPFVerified bool `json:"SPFVerified"`
 	// Host name used for the SPF configuration.
-	SPFHost string
+	SPFHost string `json:"SPFHost"`
 	// Value that can be optionally setup with your DNS host. See our
 	// [blog post](https://postmarkapp.com/blog/why-we-no-longer-ask-for-spf-records) to learn why this field is no
 	// longer necessary.
-	SPFTextValue string
+	SPFTextValue string `json:"SPFTextValue"`
 	// Specifies whether DKIM has ever been verified for the domain or not. Once DKIM is verified, this response will
 	// stay true, even if the record is later removed from DNS.
-	DKIMVerified bool
+	DKIMVerified bool `json:"DKIMVerified"`
 	// DKIM is using a strength weaker than 1024 bit. If so, it’s possible to request a new DKIM using the
 	// [RequestNewDKIM](https://postmarkapp.com/developer/api/domains-api#rotate-dkim) function below.
-	WeakDKIM bool
+	WeakDKIM bool `json:"WeakDKIM"`
 	// DNS TXT host being used to validate messages sent in
-	DKIMHost string
+	DKIMHost string `json:"DKIMHost"`
 	// DNS TXT value being used to validate messages sent in.
-	DKIMTextValue string
+	DKIMTextValue string `json:"DKIMTextValue"`
 	// If a DKIM rotation has been initiated or this DKIM is from a new Domain, this field will show the pending
 	// DKIM DNS TXT host which has yet to be setup and confirmed at your registrar or DNS host.
-	DKIMPendingHost string
+	DKIMPendingHost string `json:"DKIMPendingHost"`
 	// Similar to the DKIMPendingHost field, this will show the DNS TXT value waiting to be confirmed at your
 	// registrar or DNS host.
-	DKIMPendingTextValue string
+	DKIMPendingTextValue string `json:"DKIMPendingTextValue"`
 	// Once a new DKIM has been confirmed at your registrar or DNS host, Postmark will revoke the old DKIM host in
 	// preparation for removing it permanently from the system.
-	DKIMRevokedHost string
+	DKIMRevokedHost string `json:"DKIMRevokedHost"`
 	// Similar to DKIMRevokedHost, this field will show the DNS TXT value that will soon be removed from the Postmark system.
-	DKIMRevokedTextValue string
+	DKIMRevokedTextValue string `json:"DKIMRevokedTextValue"`
 	// Indicates whether you may safely delete the old DKIM DNS TXT records at your registrar or DNS host. The new
 	// DKIM is now safely in use.
-	SafeToRemoveRevokedKeyFromDNS bool
+	SafeToRemoveRevokedKeyFromDNS bool `json:"SafeToRemoveRevokedKeyFromDNS"`
 	// While DKIM renewal or new DKIM operations are being conducted or setup, this field will indicate Pending.
 	// After all DNS TXT records are up to date and any pending renewal operations are finished, it will indicate Verified.
-	DKIMUpdateStatus string
+	DKIMUpdateStatus string `json:"DKIMUpdateStatus"`
 	// The custom Return-Path for this domain, please [read our support page](http://support.postmarkapp.com/article/910-adding-a-custom-return-path-domain).
-	ReturnPathDomain string
+	ReturnPathDomain string `json:"ReturnPathDomain"`
 	// The verification state of the Return-Path domain. Tells you if the Return-Path is actively being used or still
 	// needs further action to be used.
-	ReturnPathDomainVerified bool
+	ReturnPathDomainVerified bool `json:"ReturnPathDomainVerified"`
 	// The CNAME DNS record that Postmark expects to find at the ReturnPathDomain value.
-	ReturnPathDomainCNAMEValue string
+	ReturnPathDomainCNAMEValue string `json:"ReturnPathDomainCNAMEValue"`
 	// Unique ID of the Domain.
 	ID int64
 }
@@ -58,11 +61,11 @@ type Domain struct {
 // DomainCreateRequest is the request body to create a domain
 type DomainCreateRequest struct {
 	// Name of the domain.
-	Name string `binding:"required"`
+	Name string `json:"Name" binding:"required"`
 	// A custom value for the Return-Path domain. It is an optional field, but it must be a subdomain of your
 	// From Email domain and must have a CNAME record that points to pm.mtasv.net. For more information about this
 	// field, please read our [support page](http://support.postmarkapp.com/article/910-adding-a-custom-return-path-domain).
-	ReturnPathDomain string
+	ReturnPathDomain string `json:"ReturnPathDomain"`
 }
 
 // DomainEditRequest is the request body to edit a domain
@@ -70,7 +73,7 @@ type DomainEditRequest struct {
 	// A custom value for the Return-Path domain. It is an optional field, but it must be a subdomain of your
 	// From Email domain and must have a CNAME record that points to pm.mtasv.net. For more information about this
 	// field, please read our [support page](http://support.postmarkapp.com/article/910-adding-a-custom-return-path-domain).
-	ReturnPathDomain string
+	ReturnPathDomain string `json:"ReturnPathDomain"`
 }
 
 // DomainsList is just a list of Domains as they are in the response
